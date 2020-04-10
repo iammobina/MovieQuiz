742c924dd391bacf2b39c6ac5b76cf003a54bdf2
iammobina <mobina.km@gmail.com>
2020-04-10 18:48:30 +0430

    Add component to show released year

--- moviequize/src/components/header.js
+++ moviequize/src/components/header.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+const getyear=() =>{
+    const newdate=new Date();
+    return newdate.getFullYear();
+}
+
 const Header=() =>{
     return(
         <div>
             <h1>Welcome to MovieQuize</h1>
+            <h2>this site started in {getyear()}</h2>
         </div>
     )
 }
