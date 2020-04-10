fd82662fe20877dad23b19dcf9503a73d04b9f59
iammobina <mobina.km@gmail.com>
2020-04-10 18:16:08 +0430

    Add myfirst component
    I wrote a component that will return a message on the screen.
    The message is:'it's gonna be LEGENDARY'

---
+++ moviequize/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () =>{
+   return <h1>it's gonna be LEGENDARY</h1>
+}
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
