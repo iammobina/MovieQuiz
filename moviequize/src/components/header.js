da2c70b6c0abe3692b982989c6e7e024a3961d54
iammobina <mobina.km@gmail.com>
2020-04-10 19:59:39 +0430

    Add Css styles to header component

--- moviequize/src/components/header.js
+++ moviequize/src/components/header.js
@@ -1,4 +1,6 @@
 import React,{Component} from "react";
+import '../css/style.css'
+
 
 const getyear=()=>{
     const newdate=new Date();
@@ -8,7 +10,7 @@ const getyear=()=>{
 class Header extends Component {
     render(){
         return(
-        <div>
+        <div className="top">
             <h1>Welcome to MovieQuize</h1>
             <h2>This site developed in {getyear()}</h2>
         </div>
